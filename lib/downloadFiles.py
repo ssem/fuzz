4fba0c64d9d5c5e29f9948f9b702b8b0c5776e91
steve <ssem@tacnetsol.com>
2014-05-02 15:31:39 -0400

    simplified downloads and findfiles

--- lib/downloadFiles.py
+++ lib/downloadFiles.py
@@ -9,106 +9,37 @@ class DownloadFiles:
         self.proxy = None
 
     def __call__(self, inputFile, outDirect):
-        lines = self.getLines(inputFile)
-        urlsAndTypes = self.getUrlsAndTypes(lines)
-        for index, url in enumerate(urlsAndTypes):
-            outputFile=self.openFile(outDirect,urlsAndTypes[url],index)
-            if not self.download(url, outputFile):
-                self.removeBadFile(outDirect,urlsAndTypes[url],index)
-            self.printStatus(index, len(urlsAndTypes))
-
-    def printStatus(self, index, total):
-        percent = Decimal(index) / Decimal(total)
-        message = 'Downloading Files: {Percent:.2%}'.format(
-            Count=index,Total=total,Percent=percent)
-        sys.stdout.write(message)
-        sys.stdout.flush()
-        sys.stdout.write('\b' * len(message))
-
-    def getLines(self, inputFile):
-        lines = []
-        f = open(inputFile, 'r')
-        for line in f:
-            if 'http://' in line and '.'+line[:line.find(':')] in line[-10:]:
-                lines.append(line.rstrip('\n'))
-        f.close()
-        return lines
-
-    def getUrlsAndTypes(self, lines):
-        urlsAndTypes = {}
-        for line in lines:
-            fileType = line[:line.find(':')]
-            url = line[line.find('http'):]
-            if fileType != '' and len(fileType) < 6:
-                urlsAndTypes[url]=fileType
-        return urlsAndTypes
-
-    def openFile(self, outDirectory, fileType, count):
-        if not outDirectory.endswith('/'):
-            outDirectory = outDirectory+'/'
-        try:
-            f = open(outDirectory+fileType+'/'+str(count)+'.'+fileType, 'w+')
-        except IOError:
-            os.makedirs(outDirectory+fileType)
-            f = open(outDirectory+fileType+'/'+str(count)+'.'+fileType, 'w+')
-        return f
-
-    def download(self, url, outputFile):
-        response = self.getResponse(url)
-        if self.filterBasedOnSize(response):
-            download = self.getPage(response)
-            if download:
+        count = 0
+        for line in open(inputFile, 'r'):
+            fileType = line[:line.find(': ')]
+            url = line[line.find(': ') + 2:]
+            data = self.getResponse(url)
+            if len(data) > 0:
+                filename = '%s.%s' % (str(count), fileType)
+                path = os.path.join(outDirect, fileType)
+                count += 1
                 try:
-                    outputFile.write(download)
-                    outputFile.close()
-                    return True
-                except:pass
-        outputFile.close()
-        return False
+                    os.makedirs(path)
+                except: pass
+                f = open(os.path.join(path, filename), 'w+')
+                f.write(data)
+                f.close()
+                message = 'Downloading Files: %s' % count
+                sys.stdout.write(message)
+                sys.stdout.flush()
+                sys.stdout.write('\b' * len(message))
 
-    def filterBasedOnSize(self, response):
-        header = self.getHeader(response)
-        if header:
-            for line in header.split('\n'):
-                if 'Content-Length' in line:
-                    length = line.split(': ')[1]
-                    if int(length) < 1000000: return True
-        return False
-
-    def removeBadFile(self, outDirectory, fileType, count):
-        try:
-            os.remove(outDirectory+fileType+'/'+str(count)+'.'+fileType)
-        except:
-            try:
-                os.remove(outDirectory+'/'+fileType+'/'+str(count)+\
-                '.'+fileType)
-            except:pass
-    
     def getResponse(self, url):
         try:
-            if self.proxy:
-                proxy = ProxyHandler({'http': self.proxy})
-                opener = build_opener(proxy)
-            else:
-                opener = build_opener()
+            opener = build_opener()
             opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-            return opener.open(str(url))
-        except: return None
-    
-    def getHeader(self, response):
-        try:
-            return str(response.info())
-        except: return None
-    
-    def getUrl(self, response):
-        try:
-            return str(response.geturl())
-        except: return None
-    
-    def getPage(self, response):
-        try:
-            return str(response.read())
-        except: return None
+            response = opener.open(str(url))
+            for line in response.info():
+                if 'Content-Length' in line:
+                    if int(line.split(': ')[1]) > 1000000:
+                        return ''
+            return response.read()
+        except: return ''
 
 
 if __name__=='__main__':
--- lib/findFiles.py
+++ lib/findFiles.py
@@ -1,126 +1,45 @@
 #!/usr/bin/env python
 import os
+import re
 import sys
 from urllib2 import *
 import google
 import random
 
 class FindFiles:
-    def __init__(self, port=80, proxy=False):
-        self.port = port
-        self.proxy = proxy
-        self.fileTypes = ['.css','.pdf','.webarchive','.webbookmark','.webhistory','.webloc',\
-        '.download','.safariextz','.gif','.htm','.js','.jpg','.jpeg','.jp2','.txt','.text',\
-        '.png','.tif','.url','.ico','.xhtml','.xht','.xml','.xbl','.xbl','.svg']
+    def __init__(self):
+        self.fileTypes = ['.css','.pdf','.webarchive','.webbookmark',
+            '.webhistory','.webloc','.download','.safariextz','.gif',
+            '.htm','.js','.jpg','.jpeg','.jp2','.txt','.text','.png',
+            '.tif','.url','.ico','.xhtml','.xht','.xml','.xbl','.xbl','.svg']
+        self.regex = '(https?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?)'
 
     def __call__(self, dictionary, output, rounds=1):
-        totalFoundItems = 0
         print '[ Loading Dictionary ]'
-        words = self.getRandomWords(dictionary)
+        f = open(dictionary, 'r')
+        words = list(f)
+        f.close()
+        f = open(output, 'w+')
         for x in xrange(int(rounds)):
             print '[ Round %s of %s]' % (str(x+1), str(rounds))
-            searchString = self.makeSearchString(words)
+            searchString = random.choice(words).rstrip('\n')
             print '    - [ Searching For %s ]' % searchString
-            urls = self.getUrls(searchString)
-            print '    - [ Found %s Urls ]' % len(urls)
-            pages = self.getPage(urls)
-            foundItems = self.filterPages(pages)
-            print '    - [ Found %s Items ]' % len(foundItems)
-            totalFoundItems += len(foundItems)
-        print '[ Total %s Items]' % totalFoundItems
-        self.saveToFile(foundItems, output)
-
-    def saveToFile(self, foundItems, output):
-        try:
-            f = open(output, 'a')
-            for item in foundItems:
-                f.write(item+'\n')
-            f.close()
-        except: pass
-
-    def getPage(self, urls):
-        pages = {}
-        for url in urls:
-            response = self.getResponse(url)
-            domain = self.getUrlFromResponse(response)
-            if response:
-                pages[self.getPageFromResponse(response)] = domain
-        return pages
-
-    def getRandomWords(self, dictionary):
-        words = []
-        f = open(dictionary, 'r')
-        for item in f:
-            words.append(item)
-        f.close()
-        if len(words) < 1:
-            print '\n\tUsage: findFiles.py      < path to dictionary>    < outfile >\n'
-            exit()
-        return words
-
-    def makeSearchString(self, words):
-        searchString = ''
-        for x in xrange(random.randrange(1,3)):
-            searchString += words[random.randrange(1,len(words))].rstrip('\n')+' '
-        return searchString.rstrip(' ')
-
-    def getUrls(self, searchString):
-        try:
-            urls = []
-            googleResults = google.search(str(searchString),stop=1)
-            for item in googleResults:
-                urls.append(item)
-            return urls
-        except:return None
-
-    def filterPages(self, pages):
-        foundItems = []
-        for page in pages:
-            try:
-                items = page.split(' ')
-                domain = pages[page]
-                for item in items:
+            for url in google.search(searchString, stop=1):
+                matches = re.findall(self.regex, self.getResponse(url))
+                print '    - [ %s  %s Matches ]' % (url, len(matches))
+                for match in matches:
                     for fileType in self.fileTypes:
-                        foundItem = self.filterPage(fileType, item, domain)
-                        if foundItem:
-                            foundItems.append(foundItem)
-            except AttributeError: pass
-        return foundItems
-
-    def filterPage(self, fileType, item, domain):
-        foundItem = None
-        if fileType in item.lower():
-            if '"' in item.lower():
-                item = item[item.find('"')+1:]
-                item = item[:item.find('"')]
-            elif "'" in item.lower():
-                item = item[item.find("'")+1:]
-                item = item[:item.find("'")]
-            if len(item) > 4:
-                if item.startswith('http:'):
-                    foundItem = fileType.lstrip('.')+': '+item
-                else:
-                    foundItem = fileType.lstrip('.')+': '+domain+item
-        return foundItem
+                        if match[0].endswith(fileType):
+                            f.write('%s: %s\n' % (fileType.lstrip('.'), match[0]))
+        f.close()
 
     def getResponse(self, url):
         try:
-            if self.proxy:
-                proxy = ProxyHandler({'http': self.proxy})
-                opener = build_opener(proxy)
-            else:opener = build_opener()
+            opener = build_opener()
             opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-            return opener.open(str(url))
-        except Exception:return None
-    def getHeaderFromResponse(self, response):
-        try:return str(response.info())
-        except: return None
-    def getUrlFromResponse(self, response):
-        try:return str(response.geturl())
-        except: return None
-    def getPageFromResponse(self, response):
-        try:return str(response.read())
-        except: return None
+            response = opener.open(str(url))
+            return response.read()
+        except Exception:return ''
 
 if __name__=='__main__':
     try:
